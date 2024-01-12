0a1a18a1473fdbca9489596275768e053e17eb46
NALYD <96333182+DylanCocchi@users.noreply.github.com>
2024-01-12 12:43:55 +0100

    Aggiornamento server-client
    Aggiornate le classi server-client

--- ConnessiooneServerClient/Client.java
+++ ConnessiooneServerClient/Client.java
@@ -1,7 +1,46 @@
 import java.io.*;
 import java.net.*;
 
-public class Client {
+public class Client extends Thread{
+
+    private String serverAddress;
+    private int port;
+
+    public Client(){
+        serverAddress = "127.0.0.1"; // Indirizzo IP del server
+        port = 5555; // Porta di connessione
+    }
+
+    public void run(){
+        try {
+            Connessione();
+        } catch (Exception e) {
+        }
+    }
+
+    public void Connessione(){
+        try {
+            // Connessione al server
+            Socket socket = new Socket(serverAddress, port);
+
+            // Lettura e scrittura dei dati
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
+
+            // Leggi dati dal server
+            String response = input.readLine();
+            System.out.println("Server: " + response);
+
+            // Invia dati al server (se necessario)
+            //output.println(dati_da_inviare);
+
+            // Chiudi la connessione
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    /* 
     public static void main(String[] args) {
         String serverAddress = "127.0.0.1"; // Indirizzo IP del server
         int port = 5555; // Porta di connessione
@@ -27,4 +66,5 @@ public class Client {
             e.printStackTrace();
         }
     }
+    */
 }
